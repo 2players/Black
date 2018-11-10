e09939e894b49ead0fd42000aa79f8bc4433647e
m31271n <m31271n@users.noreply.github.com>
2018-11-10 12:37:53 +0800

    chore: cleanup gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -12,32 +12,11 @@ gulp.task('build', function() {
     .src(files)
     .pipe(replace('/* @echo EXPORT */', 'export '))
     .pipe(sourcemaps.init())
-    .pipe(concat('black-es6-module.js'))
+    .pipe(concat('es6-module.js'))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('watch-build', ['build'], function() {
+gulp.task('build:watch', ['build'], function() {
   gulp.watch(['./src/**/*.js'], ['build'])
-})
-
-// INTERNAL TASKS
-gulp.task('copy-examples', ['build-es6'], function() {
-  return gulp
-    .src('./dist/black-es6*.*')
-    .pipe(gulp.dest('../Blacksmith-Docs/node_modules/black/dist/'))
-})
-
-gulp.task('examples', ['build-es6', 'copy-examples'], function() {
-  gulp.watch(['./src/**/*.js'], ['copy-examples'])
-})
-
-gulp.task('copy-template', ['build-es6-module'], function() {
-  return gulp
-    .src('./dist/black-es6-module.js')
-    .pipe(gulp.dest('../Black-Template/node_modules/black/dist/'))
-})
-
-gulp.task('template', ['build-es6-module', 'copy-template'], function() {
-  gulp.watch(['./src/**/*.js'], ['copy-template'])
 })
